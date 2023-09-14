a05ad190f888b1ca60a6233926a429aac4f77246
Shreyash Mhashilkar <75197447+ShreyashMhashilkar@users.noreply.github.com>
2023-09-14 20:21:11 +0530

    background updated

--- src/components/DetailForm.js
+++ src/components/DetailForm.js
@@ -23,7 +23,7 @@ const DetailForm = ({ details, setDetails, setAvailable }) => {
 
             <div  style={{backgroundImage:'url(https://images.pexels.com/photos/2101187/pexels-photo-2101187.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)'}}>
 
-                <div style={{backgroundColor:'darkblue'}} class="container py-4 px-5  border border-warning border-5 rounded">
+                <div style={{backgroundImage:'url(https://images.pexels.com/photos/956999/milky-way-starry-sky-night-sky-star-956999.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)'}} class="container py-4 px-5  border border-warning border-5 rounded">
 
                     <form className='pb-4' onSubmit={handleSubmit}>
 
